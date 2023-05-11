7d5db6a2ae7c9bc037d2e59845f9ccca70a5b662
LHXHL <1822143314@qq.com>
2023-05-11 12:03:10 +0800

    v2.0

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
 # WebStateScan
 
-**基于golang写的一个小工具，主要用于探测目标的状态。(并且根据状态分类好todo...**
+**基于golang写的一个小工具，主要用于探测目标的状态。**
 
-**用法**
+## **用法**
 
 在input.txt中放入所需要探测的URL,支持test.com,http://test.com,ip:port各种形式
 
-`go run main.go`
+```go
+git clone https://github.com/LHXHL/WebStateScan.git
+go build -o WebStateScan
+./WebStateScan -h
+```
+
+![image-20230511120011456](https://p.ipic.vip/ctr7uk.png)
+
+**彩色输出结果(-m可以静默输出，直接在文件中查看结果)**
+
+![image-20230511115748544](https://p.ipic.vip/un6n4v.png)
+
+**按照状态码从小到大的顺序排列，方便查看。**
+
+![image-20230511120132346](https://p.ipic.vip/vva6k1.png)
 
 ## ToDo...
-**改为命令flag模式的输入**
 
-**增加导出结果的功能**
+**~~改为命令flag模式的输入~~**
 
-**彩色输出？感觉挺好看的...就是有点花眼睛感觉🧐**
+~~**增加导出结果的功能**~~
+
+**~~彩色输出？感觉挺好看的...就是有点花眼睛感觉🧐~~**
+
+**还有啥功能想到了再加..**
+
--- main.go
+++ main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/csv"
+	"flag"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
 	"math/rand"
 	"net/http"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
+type flagInfo struct {
+	Silence string
+	File    string
+}
+
 type result struct {
 	State      int
-	StatusCode int
+	StatusCode int64
 	Url        string
 	Title      string
 	Length     int64
@@ -51,18 +60,21 @@ func getRandUa() string {
 	return UserAgents[index]
 }
 
-func reqScan(url string) result {
+func reqScan(url string) []result {
 	request, _ := http.NewRequest("GET", url, nil)
 	request.Header.Add("User-Agent", getRandUa())
 	resp, err := http.DefaultClient.Do(request)
+	var results []result
 	if err != nil {
-		return result{
+		r := result{
 			State:      Reject,
 			StatusCode: -1,
 			Url:        url,
 			Title:      "",
 			Length:     0,
 		}
+		results = append(results, r)
+		return results
 	}
 	defer resp.Body.Close()
 
@@ -74,7 +86,7 @@ func reqScan(url string) result {
 	doc, err := goquery.NewDocumentFromReader(teeReader)
 	if err != nil {
 		fmt.Println("解析HTML出错：", err)
-		return result{}
+		return []result{}
 	}
 	statusCode := resp.StatusCode
 	title := doc.Find("title").Text()
@@ -82,20 +94,38 @@ func reqScan(url string) result {
 	content := buf.Bytes()
 	contentLength = int64(len(content))
 
-	return result{
+	r := result{
 		State:      Survival,
-		StatusCode: statusCode,
+		StatusCode: int64(statusCode),
 		Url:        url,
 		Title:      title,
 		Length:     contentLength,
 	}
+	results = append(results, r)
+	return results
 }
 
-func scanPrint(res result) {
-	if res.State == 1 {
-		fmt.Printf("[+] StatusCode:%d Url:%s Title:%s Length:%d\n", res.StatusCode, res.Url, res.Title, res.Length)
-	} else {
-		fmt.Printf("[-] Url:%s Reject\n", res.Url)
+func infoPrint(results []result) {
+	for _, res := range results {
+		if res.State == 1 {
+			if res.StatusCode == 200 {
+				fmt.Printf("\u001B[32m[+]StatusCode:%d Url:%s Title:%s Length:%d\u001B[0m\n", res.StatusCode, res.Url, res.Title, res.Length)
+			} else if res.StatusCode == 404 {
+				fmt.Printf("\u001B[33m[+]StatusCode:%d Url:%s Title:%s Length:%d\u001B[0m\n", res.StatusCode, res.Url, res.Title, res.Length)
+
+			} else if res.StatusCode == 403 {
+				fmt.Printf("\u001B[34m[+]StatusCode:%d Url:%s Title:%s Length:%d\u001B[0m\n", res.StatusCode, res.Url, res.Title, res.Length)
+
+			} else if res.StatusCode == 500 {
+				fmt.Printf("\u001B[35m[+]StatusCode:%d Url:%s Title:%s Length:%d\u001B[0m\n", res.StatusCode, res.Url, res.Title, res.Length)
+			} else {
+				fmt.Printf("\u001B[37m[+]StatusCode:%d Url:%s Title:%s Length:%d\u001B[0m\n", res.StatusCode, res.Url, res.Title, res.Length)
+			}
+			///fmt.Printf("[+] StatusCode:%d Url:%s Title:%s Length:%d\n", res.StatusCode, res.Url, res.Title, res.Length)
+		} else {
+			fmt.Printf("\u001b[31m[-] Url:%s Reject\u001b[0m\n", res.Url)
+			//fmt.Printf("[-] Url:%s Reject\n", res.Url)
+		}
 	}
 }
 
@@ -133,7 +163,7 @@ func urlCheck(inputFile, outputFile string) error {
 	return nil
 }
 
-func fetchUrl(file string) {
+func startScan(file string, cmd *flagInfo) {
 	readFile, err := os.Open(file)
 	if err != nil {
 		fmt.Println(err)
@@ -141,7 +171,7 @@ func fetchUrl(file string) {
 	}
 	defer readFile.Close()
 
-	resultCh := make(chan result)
+	resultCh := make(chan []result)
 	wg := sync.WaitGroup{}
 	scanner := bufio.NewScanner(readFile)
 	for scanner.Scan() {
@@ -156,19 +186,152 @@ func fetchUrl(file string) {
 		wg.Wait()
 		close(resultCh)
 	}()
-
+	filename := "out.csv"
 	for req := range resultCh {
-		scanPrint(req)
+		if cmd.File != "" {
+			infoPrint(req)
+			resultMap := cateByStatusCode(req)
+			err := write2Csv(filename, resultMap)
+			if err != nil {
+				return
+			}
+		} else if cmd.Silence != "" {
+			resultMap := cateByStatusCode(req)
+			err := write2Csv(filename, resultMap)
+			if err != nil {
+				return
+			}
+		}
 	}
 }
 
-func main() {
-	input := "input.txt"
-	output := "output.txt"
-	err := urlCheck(input, output)
+func Flag(cmd *flagInfo) {
+	flag.StringVar(&cmd.File, "f", "", "Batch URL Detection, for example: urls.txt")
+	flag.StringVar(&cmd.Silence, "m", "", "Batch URL Detection(silence ouput), for example: urls.txt")
+	flag.Parse()
+}
+
+func cateByStatusCode(results []result) map[int64][]result {
+	resultMap := make(map[int64][]result)
+	for _, r := range results {
+		resultMap[r.StatusCode] = append(resultMap[r.StatusCode], r)
+	}
+	return resultMap
+}
+
+func write2Csv(filename string, resultMap map[int64][]result) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	if err != nil {
+		return err
+	}
+	for _, results := range resultMap {
+		for _, r := range results {
+			err := writer.Write([]string{strconv.FormatInt(r.StatusCode, 10), r.Url, r.Title, strconv.FormatInt(r.Length, 10)})
+			if err != nil {
+				return err
+			}
+		}
+	}
+	writer.Flush()
+	return nil
+}
+
+func writeCsvHeader(filename string) {
+	csvFile, err := os.Create(filename)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fetchUrl(output)
+	defer csvFile.Close()
+	writer := csv.NewWriter(csvFile)
+	err = writer.Write([]string{"StatusCode", "URL", "Title", "Length"})
+	writer.Flush()
+}
+
+func dealCsv(filename string, output string) {
+	csvFile, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	// 确定要排序的列的索引
+	columnIndex := -1
+	for i, colName := range records[0] {
+		if colName == "StatusCode" {
+			columnIndex = i
+			break
+		}
+	}
+	if columnIndex == -1 {
+		panic("column StatusCode not found")
+	}
+
+	sortFunc := func(i, j int) bool {
+		return records[i+1][columnIndex] < records[j+1][columnIndex]
+	}
+
+	sort.SliceStable(records[1:], sortFunc)
+
+	outfile, err := os.Create(output)
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	writer := csv.NewWriter(outfile)
+	defer writer.Flush()
+
+	for _, row := range records {
+		err := writer.Write(row)
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Printf("结果保存在文件:%s\n", output)
+}
+
+func main() {
+	var cmd flagInfo
+	Flag(&cmd)
+	output := "output.txt"
+	filename := "out.csv"
+	writeCsvHeader(filename)
+	var outCsv string
+	if cmd.File != "" {
+		err := urlCheck(cmd.File, output)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("请输入导出结果的文件名(xxx.csv):")
+		fmt.Scanln(&outCsv)
+		startScan(output, &cmd)
+		dealCsv(filename, outCsv)
+	} else if cmd.Silence != "" {
+		err := urlCheck(cmd.Silence, output)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("请输入导出结果的文件名(xxx.csv):")
+		fmt.Scanln(&outCsv)
+		fmt.Println("[+]Detecting...")
+		startScan(output, &cmd)
+		dealCsv(filename, outCsv)
+	} else {
+		fmt.Println("Please provide a argument (-h/-f/-m)")
+	}
 }
